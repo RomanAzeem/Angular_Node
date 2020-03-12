7e66855783b1c1f3c1731345c9c11958322eea3f
RomanAzeem <romanazeem007@gmail.com>
2020-03-12 07:05:25 +0500

    database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoAltasURI');
 
-const mongoAltasURI ="mongodb+srv://test123:test123@devconnector-8a5we.mongodb.net/node-angular?retryWrites=true&w=majority";
+// const mongoAltasURI ="mongodb+srv://test123:test123@devconnector-8a5we.mongodb.net/node-angular?retryWrites=true&w=majority";
 const connectDB = async () => {
   try {
-    console.log(mongoAltasURI);
-    await mongoose.connect(mongoAltasURI, {
+    console.log(db);
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     });
-    console.log('MongoDB Connected...'+mongoAltasURI);
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
     //Exit Process with Failure
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
